90d1b5c62be43995c3b92f2a83248139509c31de
Jorge Benites <jorge.benaljo23@gmail.com>
2025-05-12 11:05:28 -0500

    feat: Avance en Footer y header

--- src/app/public/components/public-header/public-header.component.ts
+++ src/app/public/components/public-header/public-header.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-public-header',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [MenubarModule,BadgeModule,CommonModule,AvatarModule],
   templateUrl: './public-header.component.html',
   styleUrl: './public-header.component.scss'
 })
 export class PublicHeaderComponent {
-
+items = [
+  { label: 'Inicio', styleClass:'text-style' },
+  { label: 'Cotización' , styleClass:'text-style'},
+  { label: 'Sobre nosotros' , styleClass:'text-style'},
+];
 }
