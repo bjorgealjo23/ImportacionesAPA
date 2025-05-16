24646b4aa825828e1fbe3acd70e4a3f488d69d10
Jorge Benites <jorge.benaljo23@gmail.com>
2025-05-16 12:28:29 -0500

    feat: subiendo mejoras en cotizacion

--- src/app/public/landing/components/cotizacion/cotizacion.component.ts
+++ src/app/public/landing/components/cotizacion/cotizacion.component.ts
@@ -1,11 +1,4 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CardModule } from 'primeng/card';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { FloatLabelModule } from 'primeng/floatlabel';
-import { ButtonModule } from 'primeng/button';
 import { FormularioCotizacionComponent } from './components';
 
 
