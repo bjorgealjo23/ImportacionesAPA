463099a267ed32dbd7f1c77d7596152d2e4e79af
Jorge Benites <jorge.benaljo23@gmail.com>
2025-05-16 17:48:45 -0500

    subiendo avance de responsivo e iteraccion con agreagdo de embarques

--- src/app/public/landing/components/cotizacion/components/formulario-cotizacion/formulario-cotizacion.component.ts
+++ src/app/public/landing/components/cotizacion/components/formulario-cotizacion/formulario-cotizacion.component.ts
@@ -1,19 +1,70 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
 
+export interface embarqueProp {
+  id: number;
+  numero: number;
+  precio: number;
+  cantidad: number;
+  peso: number;
+}
+
 @Component({
   selector: 'app-formulario-cotizacion',
   standalone: true,
-  imports: [CardModule,InputTextModule, CommonModule,FormsModule,FloatLabelModule,ButtonModule,InputNumberModule],
+  imports: [CardModule, InputTextModule, CommonModule, FloatLabelModule, ButtonModule, InputNumberModule, ReactiveFormsModule, FormsModule],
   templateUrl: './formulario-cotizacion.component.html',
   styleUrl: './formulario-cotizacion.component.scss'
 })
 export class FormularioCotizacionComponent {
-    value:any ;
+  value: any;
+  lstEmbarques: embarqueProp[] = [];
+
+
+  ngOnInit(): void {
+
+    this.lstEmbarques = [{
+      id: this.generarId(),
+      numero: 1,
+      precio: 0,
+      cantidad: 0,
+      peso: 0
+    }];
+
+  }
+  generarId(): number {
+    return Math.floor(1000 + Math.random() * 9000);
+  }
+
+  agregarNuevoEmbarque() {
+    this.lstEmbarques = [... this.lstEmbarques, {
+      id: this.generarId(),
+      numero: this.lstEmbarques.length + 1,
+      precio: 0,
+      cantidad: 0,
+      peso: 0
+    }]
+  }
+
+  eliminarFilaEmbarque(id: number) {
+    if (this.lstEmbarques.length > 1) {
+      const index = this.lstEmbarques.findIndex(item => item.id === id);
+      if (index !== -1) {
+        this.lstEmbarques.splice(index, 1);
+        this.lstEmbarques = [...this.lstEmbarques];
+      }
+    }
+  }
+
+
+
+
+
+
 }
