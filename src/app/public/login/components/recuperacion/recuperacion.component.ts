5e5f5e406e08d890b250f019ca11595b2872e828
Jorge Benites <jorge.benaljo23@gmail.com>
2025-05-19 17:53:25 -0500

    subiendo avance de componente de recuperacion de contraseña

--- src/app/public/login/components/recuperacion/recuperacion.component.ts
+++ src/app/public/login/components/recuperacion/recuperacion.component.ts
@@ -1,14 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @Component({
   selector: 'app-recuperacion',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule,InputNumberModule,FloatLabelModule,ButtonModule,CommonModule,InputTextareaModule, CommonModule, FormsModule,RouterModule],
   templateUrl: './recuperacion.component.html',
   styleUrl: './recuperacion.component.scss'
 })
 export class RecuperacionComponent {
-
+value:any;
 }
 
 
