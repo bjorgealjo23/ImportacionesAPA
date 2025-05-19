c2f3a6eb3267a9a56394d02dc74d0056501b1373
Jorge Benites <jorge.benaljo23@gmail.com>
2025-05-19 17:37:49 -0500

    feat: subiendo avance de maquetacion login y registro

--- src/app/public/login/components/inicio/inicio.component.ts
+++ src/app/public/login/components/inicio/inicio.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule,InputNumberModule,FloatLabelModule,ButtonModule,CommonModule,InputTextareaModule, CommonModule, FormsModule,RouterModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
 export class InicioComponent {
 
+  value:any;
+  verPassword:boolean = false;
 }
