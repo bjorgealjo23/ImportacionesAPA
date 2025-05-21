c9c87550a65cd6d1ec240f2269ae74c2ddb21a68
Jorge Benites <jorge.benaljo23@gmail.com>
2025-05-21 12:31:51 -0500

    feat:subiendo avance maquetado dashboard

--- src/app/dashboard/admin/components/bienvenido/bienvenido.component.ts
+++ src/app/dashboard/admin/components/bienvenido/bienvenido.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-bienvenido',
   standalone: true,
-  imports: [],
+  imports: [CardModule,InputTextModule, CommonModule,FormsModule,FloatLabelModule],
   templateUrl: './bienvenido.component.html',
   styleUrl: './bienvenido.component.scss'
 })
