5e25768f55af2bb10794276e82c202510873e9ed
Jorge Benites <jorge.benaljo23@gmail.com>
2025-05-23 17:18:20 -0500

    subienfo form de perfil

--- src/app/dashboard/admin/components/perfil/perfil.component.ts
+++ src/app/dashboard/admin/components/perfil/perfil.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule,InputTextModule,InputNumberModule,FloatLabelModule,ButtonModule],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.scss'
 })
 export class PerfilComponent {
-
+  value:any;
 }
